1ce98206622d50300c16a499142fa406036f7e3e
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-02-03 18:00:47 -0500

    make env optional

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,16 +6,19 @@ import { z } from 'zod'
  * with invalid env vars.
  */
 export const serverSchema = z.object({
-	DATABASE_URL: z.string().url(),
+	DATABASE_URL: z.string().url().optional(),
 	NODE_ENV: z.enum(['development', 'test', 'production']),
-	NEXTAUTH_SECRET: process.env.NODE_ENV === 'production' ? z.string().min(1) : z.string().min(1).optional(),
-	NEXTAUTH_URL: z.preprocess(
-		// This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-		// Since NextAuth.js automatically uses the VERCEL_URL if present.
-		(str) => process.env.VERCEL_URL ?? str,
-		// VERCEL_URL doesn't include `https` so it cant be validated as a URL
-		process.env.VERCEL ? z.string() : z.string().url()
-	),
+	NEXTAUTH_SECRET:
+		process.env.NODE_ENV === 'production' ? z.string().min(1).optional() : z.string().min(1).optional(),
+	NEXTAUTH_URL: z
+		.preprocess(
+			// This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+			// Since NextAuth.js automatically uses the VERCEL_URL if present.
+			(str) => process.env.VERCEL_URL ?? str,
+			// VERCEL_URL doesn't include `https` so it cant be validated as a URL
+			process.env.VERCEL ? z.string() : z.string().url()
+		)
+		.optional(),
 })
 
 /**
