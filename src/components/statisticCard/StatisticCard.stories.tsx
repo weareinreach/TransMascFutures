51f318373af720d6b1801a8886443cd83b2d2967
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-02-08 21:44:10 -0400

    code refactoring, changed default story, added new story, added controls to stories

--- src/components/statisticCard/StatisticCard.stories.tsx
+++ src/components/statisticCard/StatisticCard.stories.tsx
@@ -1,45 +1,56 @@
 import { Flex } from '@mantine/core'
-import React from 'react'
 
 import { StatisticCard } from './StatisticCard'
 
-import type { StoryFn, Meta } from '@storybook/react'
+import type { cardProps } from './StatisticCard'
+import type { Meta } from '@storybook/react'
 
 export default {
-	title: 'Statistic Cards',
+	title: 'components/Statistic Cards',
 	component: StatisticCard,
 	parameters: {
 		layout: 'fullscreen',
 	},
+	tags: ['autodocs'],
 } as Meta<typeof StatisticCard>
 
-const Template: StoryFn<typeof StatisticCard> = () => {
-	const cardsInfo = [
-		{
-			title: '59%',
-			text: 'Of trans men and masculine youth have considered suicide.',
-		},
-		{
-			title: '1 in 5',
-			text: 'Transgender and nonbinary youth have attempted suicide',
-		},
-		{
-			title: '36%',
-			text: 'Of the U.S. trans population identify as trans men.',
-		},
-	]
+const statistics = [
+	{
+		title: '59%',
+		text: 'Of trans men and masculine youth have considered suicide.',
+	},
+	{
+		title: '1 in 5',
+		text: 'Transgender and nonbinary youth have attempted suicide',
+	},
+	{
+		title: '36%',
+		text: 'Of the U.S. trans population identify as trans men.',
+	},
+]
 
-	const cards = cardsInfo.map((card) => <StatisticCard key={card.text} title={card.title} text={card.text} />)
+export const Default = ({ title, text }: cardProps) => <StatisticCard title={title} text={text} />
+Default.args = {
+	title: statistics[0]?.title,
+	text: statistics[0]?.text,
+}
 
+export const AboutPage = ({ cards }: storyArgs) => {
+	const children = cards.map(({ title, text }) => <StatisticCard key={title} title={title} text={text} />)
 	return (
 		<Flex
 			direction={{ base: 'column', md: 'row' }}
 			justify={{ md: 'center' }}
 			align={{ base: 'center', md: 'start' }}
 		>
-			{cards}
+			{children}
 		</Flex>
 	)
 }
+AboutPage.args = {
+	cards: statistics,
+}
 
-export const Default = Template.bind({})
+type storyArgs = {
+	cards: cardProps[]
+}
