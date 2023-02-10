47f0c667081fd6d6a31b24eb4ef4b7d3c341babb
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-02-10 02:22:47 -0400

    added MainPage to index.tsx page, use trpc query to get 9 most recent stories, display loader while data is undefined and display error message if query fails

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
+import { Loader, Center, Title, Flex } from '@mantine/core'
+
+import { MainPage } from '../layouts/MainPage/MainPage'
+import { api } from '../utils/api'
+
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<div>Index site</div>
-		</>
-	)
+	const { data, status } = api.story.recentNine.useQuery()
+	if (data === undefined) {
+		return (
+			<Center style={{ width: '100%', height: '100%' }}>
+				{status === 'error' ? (
+					<Flex direction={'column'} align='center'>
+						<Title order={1} ta='center'>
+							{'Ooops something went wrong :('}
+						</Title>
+						<Title order={2} ta='center'>
+							{'Try refreshing the page'}
+						</Title>
+					</Flex>
+				) : (
+					<Loader variant='bars' size='xl' />
+				)}
+			</Center>
+		)
+	} else {
+		return <MainPage stories={data} />
+	}
 }
 
 export default Home
