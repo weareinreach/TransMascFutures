a17367379d95019f919574b3f675ef4ddce73c20
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-02-10 01:55:13 -0400

    added a static image to rely in case story doesn't have either a default image or a user uploaded image

--- src/components/storyPreviewCard/PreviewCard.tsx
+++ src/components/storyPreviewCard/PreviewCard.tsx
@@ -1,11 +1,12 @@
-import { Card, Text, createStyles, AspectRatio } from '@mantine/core'
+import { Card, Text, createStyles, AspectRatio, Skeleton } from '@mantine/core'
 import Image from 'next/image'
+import noImage from '/public/assets/COLOR_TRANSMASCFUTURES (500x500).png'
 
 type CardProps = {
 	title: string
 	text: string
 	imgAlt: string
-	imgSrc: string
+	imgSrc?: string
 }
 
 const useStyles = createStyles(() => ({
@@ -21,7 +22,7 @@ export const PreviewCard = ({ text, title, imgSrc, imgAlt }: CardProps) => {
 		<Card m='md' h='90%' className={classes.card}>
 			<Card.Section>
 				<AspectRatio ratio={480 / 355}>
-					<Image width={480} height={355} alt={imgAlt} src={imgSrc} />
+					<Image width={480} height={355} alt={imgAlt} src={imgSrc || noImage} />
 				</AspectRatio>
 			</Card.Section>
 			<Text fw={700} fz='lg'>
