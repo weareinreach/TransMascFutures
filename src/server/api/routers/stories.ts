f9cfe341fe5d04ec1fc2e99a98a6e91a8d072aa6
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-02-11 02:43:54 -0400

    recentNine query can accept an optional parameter 'category' which can be used to scope the retrieve the 9 most recent stories from a specific category

--- src/server/api/routers/stories.ts
+++ src/server/api/routers/stories.ts
@@ -3,15 +3,23 @@ import { z } from 'zod'
 import { createTRPCRouter, publicProcedure, protectedProcedure } from '../trpc'
 
 export const storyRouter = createTRPCRouter({
-	recentNine: publicProcedure.query(({ ctx }) =>
-		ctx.prisma.story.findMany({
-			where: {
-				published: true,
-			},
-			take: 9,
-			orderBy: {
-				createdAt: 'desc',
-			},
-		})
-	),
+	recentNine: publicProcedure
+		.input(
+			z
+				.object({
+					category: z.enum(['queer', 'bipoc', 'disabled']).nullish(),
+				})
+				.nullish()
+		)
+		.query(({ ctx, input }) => {
+			let filter = { published: true }
+			if (input && input.category) filter = { ...filter, ...{ keyJoy: input.category } }
+
+			return ctx.prisma.story.findMany({
+				where: filter,
+				take: 9,
+				orderBy: { createdAt: 'desc' },
+				include: { defaultImage: true },
+			})
+		}),
 })
