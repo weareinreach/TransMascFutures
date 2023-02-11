bba7b65b904153c337b073384a758c77c810317b
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-02-11 16:34:07 -0400

    remove unused imports

--- src/server/api/routers/stories.ts
+++ src/server/api/routers/stories.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-import { createTRPCRouter, publicProcedure, protectedProcedure } from '../trpc'
+import { createTRPCRouter, publicProcedure } from '../trpc'
 
 export const storyRouter = createTRPCRouter({
 	recentNine: publicProcedure
