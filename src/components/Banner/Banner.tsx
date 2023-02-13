1b78434b6a17937281772e5fef420e3223c96f4e
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-02-13 19:50:58 -0400

    source

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,24 @@
+import { Container, Grid, Title, AspectRatio } from '@mantine/core'
+import Image from 'next/image'
+
+export const Banner = ({ title }: BannerProp) => {
+	return (
+		<Container fluid p={'xl'}>
+			<Grid px='xl'>
+				<Grid.Col pl='xl' lg={3} md={12}>
+					<Title fw={900} order={1} size='30px !important' tt='uppercase'>
+						{'Act'}
+					</Title>
+				</Grid.Col>
+				<Grid.Col span='auto'>
+					<AspectRatio ratio={800 / 300}>
+						<Image src='/assets/tmf-logo-rect-bw.png' alt='transmasc logo' width={800} height={300} />
+					</AspectRatio>
+				</Grid.Col>
+				<Grid.Col lg={3} md={12}></Grid.Col>
+			</Grid>
+		</Container>
+	)
+}
+
+type BannerProp = { title: string }
