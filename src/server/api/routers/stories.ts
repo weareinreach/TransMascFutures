afc572afac4ac389027089d0d7bb5ddd84817c1c
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-02-13 17:33:37 -0400

    added getStoryById and getStoryBySlug procedures

--- src/server/api/routers/stories.ts
+++ src/server/api/routers/stories.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-import { createTRPCRouter, publicProcedure } from '../trpc'
+import { createTRPCRouter, publicProcedure, protectedProcedure } from '../trpc'
 
 export const storyRouter = createTRPCRouter({
 	recentNine: publicProcedure
@@ -11,15 +11,40 @@ export const storyRouter = createTRPCRouter({
 				})
 				.nullish()
 		)
-		.query(({ ctx, input }) => {
+		.query(async ({ ctx, input }) => {
 			let filter = { published: true }
 			if (input && input.category) filter = { ...filter, ...{ keyJoy: input.category } }
 
-			return ctx.prisma.story.findMany({
+			const stories = await ctx.prisma.story.findMany({
 				where: filter,
 				take: 9,
 				orderBy: { createdAt: 'desc' },
 				include: { defaultImage: true },
 			})
+			return stories
+		}),
+	getStoryBySlug: publicProcedure
+		.input(
+			z.object({
+				publicSlug: z.string(),
+			})
+		)
+		.query(async ({ ctx, input }) => {
+			const story = await ctx.prisma.story.findUnique({
+				where: { publicSlug: input.publicSlug },
+			})
+			return story
+		}),
+	getStoryById: protectedProcedure
+		.input(
+			z.object({
+				id: z.string(),
+			})
+		)
+		.query(async ({ ctx, input }) => {
+			const story = await ctx.prisma.story.findUnique({
+				where: { id: input.id },
+			})
+			return story
 		}),
 })
