819c7e94d9b8e4883c2e81ba98c3bfe11bad5d3b
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-06-02 22:22:47 -0400

    image assets

---
+++ src/data/categoryImages.ts
@@ -0,0 +1,20 @@
+import bipoc from '../../public/assets/characters/cropped/bipoc.png'
+import disabled from '../../public/assets/characters/cropped/disabled.png'
+import elder from '../../public/assets/characters/cropped/elder.png'
+import immigrant from '../../public/assets/characters/cropped/immigrant.png'
+import queer from '../../public/assets/characters/cropped/queer.png'
+import transman from '../../public/assets/characters/cropped/transman.png'
+import transmasc from '../../public/assets/characters/cropped/transmasc.png'
+
+export const categoryImages = {
+	bipoc,
+	disabled,
+	elder,
+	immigrant,
+	queer,
+	transman,
+	transmasc,
+} as const
+
+export const isValidCategoryImage = (category: unknown): category is keyof typeof categoryImages =>
+	typeof category === 'string' && Object.hasOwn(categoryImages, category)
