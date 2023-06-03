cdbbc258e26dea9e8b60de090c673861175e990f
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-06-02 22:26:00 -0400

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,41 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
 // @ts-check
+import bundleAnalyze from '@next/bundle-analyzer'
+import nextRoutes from 'nextjs-routes/config'
+
 import i18nConfig from './next-i18next.config.js'
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful for
  * Docker builds.
  */
+// eslint-disable-next-line turbo/no-undeclared-env-vars
 !process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'))
+
+const withRoutes = nextRoutes({ outDir: 'src/types' })
+const withBundleAnalyzer = bundleAnalyze({ enabled: process.env.ANALYZE === 'true' })
 
 /** @type {import('next').NextConfig} */
 const config = {
 	i18n: i18nConfig.i18n,
 	reactStrictMode: true,
 	swcMinify: true,
+	compiler: {
+		...(process.env.VERCEL_ENV === 'production' ? { removeConsole: { exclude: ['error'] } } : {}),
+	},
+	images: {
+		remotePatterns: [{ protocol: 'https', hostname: 'placehold.co', pathname: '/**' }],
+		// domains: ['placehold.co'],
+	},
 }
-export default config
+/**
+ * Wraps NextJS config with the Bundle Analyzer config.
+ *
+ * @template {typeof config} T
+ * @param {T} config
+ * @returns {T}
+ */
+function defineNextConfig(config) {
+	return withBundleAnalyzer(withRoutes(config))
+}
+
+export default defineNextConfig(config)
