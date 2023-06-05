fa3696c1023063497515f8a48c37995a37a8d293
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-06-04 22:12:39 -0400

    share page

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -1,91 +1,107 @@
-import { AspectRatio, Button, createStyles, Flex, List, rem, Stack } from '@mantine/core'
+import {
+	AspectRatio,
+	Button,
+	Center,
+	Container,
+	createStyles,
+	em,
+	Grid,
+	List,
+	rem,
+	Stack,
+	Text,
+} from '@mantine/core'
 import { type GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link'
 import { useTranslation } from 'next-i18next'
 
+import { Banner } from '~/components'
 import { getServerSideTranslations } from '~/server/i18n'
-import Logo from '~public/assets/tmf-logo-rect-bw.png'
+import ShareImage from '~public/assets/share.jpg'
 
 const useStyles = createStyles((theme) => ({
 	header: {
 		textTransform: 'uppercase',
 	},
 
-	flexWrapper: {
-		padding: `${rem(0)} ${rem(15)}`,
-		['& *']: {
-			flex: 1,
-		},
-		[theme.fn.smallerThan('md')]: {
-			flexDirection: 'column',
-		},
-	},
 	content: {
-		gap: rem(0),
-		margin: rem(0),
-		padding: `${rem(0)} ${rem(25)}`,
 		textAlign: 'center',
-	},
-	downloadText: {
-		margin: rem(0),
-		textTransform: 'uppercase',
-		fontSize: '1.5rem',
-		fontWeight: 'bold',
+		gap: rem(20),
+		[theme.fn.largerThan('md')]: {
+			gap: rem(60),
+		},
 	},
 	listItem: {
-		fontSize: '1.2rem',
-		listStyleType: 'none',
+		fontSize: rem(20),
+		textAlign: 'start',
+		[theme.fn.smallerThan('md')]: {
+			fontSize: rem(16),
+		},
+		[theme.fn.smallerThan(em(820))]: {
+			fontSize: rem(14),
+		},
+		[theme.fn.smallerThan('sm')]: {
+			fontSize: rem(16),
+		},
 	},
 }))
-const SharePage = ({ image }: Props) => {
+const SharePage = () => {
 	const { t } = useTranslation()
 	const { classes } = useStyles()
 	return (
-		<Stack style={{ gap: 0, margin: '25px 50px' }}>
+		<>
 			<Head>
 				<title>{t('page-title.general-template', { page: '$t(nav.share)' })}</title>
 			</Head>
-			<h2 className={classes.header}>Share</h2>
-			<Flex className={classes.flexWrapper} direction='row' align='center'>
-				<Stack>
-					<Image
-						src={image}
-						style={{ marginBottom: '50px' }}
-						alt='individual representing transmasc futures'
-					/>
-					<Link href='/'>
-						<Button>Back to Home</Button>
-					</Link>
-				</Stack>
-				<Stack className={classes.content} align='center'>
-					<AspectRatio ratio={723 / 174} my={40} mx='auto' maw='70vw'>
-						<Image src={Logo} alt={t('logo-alt')} fill />
-					</AspectRatio>
-					<p className={classes.downloadText} style={{ margin: 0 }}>
-						Download our #transmascfutures social media toolkit.
-					</p>
-					<p style={{ fontSize: '1.4rem', fontStyle: 'italic', fontWeight: 'bold', margin: 0 }}>
-						For your sharing convenience:
-					</p>
-					<List style={{ marginBottom: '25px' }}>
-						<List.Item className={classes.listItem}>&gt; Three 1080 x1080 PNG Graphics</List.Item>
-						<List.Item className={classes.listItem}>
-							&gt; Word document with recommended sharing language
-						</List.Item>
-						<List.Item className={classes.listItem}>&gt; Press packet</List.Item>
-					</List>
-					<Button>Download ZIP File</Button>
-				</Stack>
-			</Flex>
-		</Stack>
+			{/* <Group style={{ gap: 0, margin: '25px 50px' }}> */}
+			<Container fluid>
+				<Banner titleKey='nav.share' />
+				<Grid>
+					<Grid.Col sm={6}>
+						{/* <Stack justify='center' align='center'>
+						<h2 className={classes.header}>{t('nav.share')}</h2> */}
+						<Center>
+							<AspectRatio ratio={3 / 4} w={`min(calc(${ShareImage.height}px * .75), calc(70vh *.75))`}>
+								<Image
+									src={ShareImage}
+									style={{ marginBottom: '50px' }}
+									alt='individual representing transmasc futures'
+								/>
+							</AspectRatio>
+						</Center>
+						{/* </Stack> */}
+					</Grid.Col>
+					<Grid.Col sm={6} h='100%' my='auto'>
+						<Stack className={classes.content} align='center'>
+							<Stack spacing={8}>
+								<Text fz={{ base: rem(20), md: rem(24) }} tt='uppercase' fw={600}>
+									{t('share.title')}
+								</Text>
+								<Text
+									// style={{ fontSize: '1.4rem', fontStyle: 'italic', fontWeight: 'bold', margin: 0 }}
+									fz={{ base: rem(18), md: rem(22) }}
+									fs='italic'
+									fw={600}
+								>
+									{t('share.subtitle')}
+								</Text>
+							</Stack>
+							<List py={20} icon='🏳️‍⚧️' classNames={{ item: classes.listItem }}>
+								<List.Item>{t('share.list1')}</List.Item>
+								<List.Item>{t('share.list2')}</List.Item>
+							</List>
+							<Button component='a' href='/assets/media-kit.zip' target='_blank' rel='noopener noreferrer'>
+								{t('share.download-button')}
+							</Button>
+						</Stack>
+					</Grid.Col>
+				</Grid>
+			</Container>
+		</>
 	)
 }
 
-type Props = {
-	image: string
-}
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
 	return {
 		props: {
