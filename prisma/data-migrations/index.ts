920d255839385c5892b6bc32846708e3cc1c0bdb
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-06-20 10:43:33 -0400

    data migration: add stories

--- prisma/data-migrations/index.ts
+++ prisma/data-migrations/index.ts
@@ -1,3 +1,4 @@
 // codegen:start {preset: barrel, include: ./**/*.ts, exclude: ./**/!*.ts}
 export * from './2023-06-12_add-stories/index'
+export * from './2023-06-20_add-stories/index'
 // codegen:end
