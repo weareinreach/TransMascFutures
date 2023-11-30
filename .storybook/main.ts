28b8730a44484e278aa8a5fc51ab96666ab58a94
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-11-30 13:20:39 -0500

    update storybook conf

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,8 @@
 import { type StorybookConfig } from '@storybook/nextjs'
 import { merge } from 'merge-anything'
 
+import { dirname, join } from 'path'
+
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	staticDirs: [
@@ -10,10 +12,10 @@ const config: StorybookConfig = {
 		},
 	],
 	addons: [
-		'@storybook/addon-a11y',
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
+		getAbsolutePath('@storybook/addon-a11y'),
+		getAbsolutePath('@storybook/addon-links'),
+		getAbsolutePath('@storybook/addon-essentials'),
+		getAbsolutePath('@storybook/addon-interactions'),
 	],
 	framework: {
 		name: '@storybook/nextjs',
@@ -47,3 +49,7 @@ const config: StorybookConfig = {
 }
 
 export default config
+
+function getAbsolutePath(value: string): string {
+	return dirname(require.resolve(join(value, 'package.json')))
+}
