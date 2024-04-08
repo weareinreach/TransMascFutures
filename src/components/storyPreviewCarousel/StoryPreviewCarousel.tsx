59328b30c8c7f6c89e666d230c7c42368ae2388a
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-04-08 18:05:44 -0400

    story preview carousel

--- src/components/storyPreviewCarousel/StoryPreviewCarousel.tsx
+++ src/components/storyPreviewCarousel/StoryPreviewCarousel.tsx
@@ -1,21 +1,17 @@
 import { Carousel, type CarouselProps } from '@mantine/carousel'
-import { createStyles, rem } from '@mantine/core'
 import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react'
 import AutoHeight from 'embla-carousel-auto-height'
-import { forwardRef, type ReactNode } from 'react'
+import { Children, forwardRef, type ReactNode } from 'react'
+
+import classes from './StoryPreviewCarousel.module.css'
 
 interface StoryPreviewCarouselProps extends CarouselProps {
 	children: ReactNode[]
 }
-const useStyles = createStyles((theme) => ({
-	viewport: {
-		padding: `${rem(60)} ${rem(0)}`,
-	},
-}))
+
 export const StoryPreviewCarousel = forwardRef<HTMLDivElement, StoryPreviewCarouselProps>(
 	({ children, ...props }, ref) => {
-		const { classes } = useStyles()
-		const slides = children.map((child, i) => <Carousel.Slide key={i}>{child}</Carousel.Slide>)
+		const slides = Children.map(children, (child, i) => <Carousel.Slide key={i}>{child}</Carousel.Slide>)
 		const [size, strokeWidth, color] = [64, 3, 'black']
 		return (
 			<Carousel
@@ -28,19 +24,13 @@ export const StoryPreviewCarousel = forwardRef<HTMLDivElement, StoryPreviewCarou
 				slidesToScroll={1}
 				nextControlIcon={<IconChevronRight size={size} strokeWidth={strokeWidth} color={color} />}
 				previousControlIcon={<IconChevronLeft size={size} strokeWidth={strokeWidth} color={color} />}
-				styles={(theme) => ({
-					control: {
-						border: 'none',
-						backgroundColor: 'transparent',
-						boxShadow: 'none',
-					},
-				})}
+				className={classes.carousel}
 				// controlsOffset={16}
 				inViewThreshold={1}
 				plugins={[AutoHeight({ active: true })]}
 				controlSize={size}
 				classNames={classes}
-				sx={{ flex: 1 }}
+				style={{ flex: 1 }}
 				{...props}
 			>
 				{slides}
