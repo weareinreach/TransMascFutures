64acf0aa72578b1b21c93f36958b6d5351c36ce2
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-04-08 18:05:17 -0400

    stat card

--- src/components/statisticCard/StatisticCard.tsx
+++ src/components/statisticCard/StatisticCard.tsx
@@ -1,24 +1,6 @@
-import { Card, createStyles, rem, Text, Title } from '@mantine/core'
+import { Card, Text, Title } from '@mantine/core'
 
-const useStyles = createStyles((theme) => ({
-	card: {
-		maxWidth: rem(320),
-	},
-	title: {
-		marginBottom: `${rem(0)} !important`,
-		[theme.fn.smallerThan('md')]: {
-			fontSize: rem(24),
-		},
-	},
-	text: {
-		paddingLeft: theme.spacing.xl,
-		paddingRight: theme.spacing.xl,
-		fontSize: theme.fontSizes.xl,
-		[theme.fn.smallerThan('md')]: {
-			fontSize: rem(18),
-		},
-	},
-}))
+import classes from './StatisticCard.module.css'
 
 export type cardProps = {
 	title: string
@@ -26,11 +8,10 @@ export type cardProps = {
 }
 
 export const StatisticCard = ({ title, text }: cardProps) => {
-	const { classes } = useStyles()
 	return (
 		<Card className={classes.card}>
 			<Card.Section>
-				<Title order={1} weight={700} align='center' className={classes.title}>
+				<Title order={1} fw={700} ta='center' className={classes.title}>
 					{title}
 				</Title>
 			</Card.Section>
