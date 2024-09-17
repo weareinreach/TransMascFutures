f92ae20975375e10e7b165818fe7672afce1d322
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-09-17 17:02:54 -0400

    migrated component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,7 +3,7 @@
 export * from './BackHomeButton/BackHomeButton'
 export * from './Banner/Banner'
 export * from './ModalForm/ModalForm'
-export * from './Navbar/Navbar'
+
 export * from './statisticCard/StatisticCard'
 export * from './storyPreviewCard/PreviewCard'
 export * from './storyPreviewCarousel/StoryPreviewCarousel'
