469c6009d9decce43b8b8433cd8f1a2375026dc2
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-09-26 12:49:15 -0400

    extract banner to component

--- src/app/[locale]/category/[...tag]/page.tsx
+++ src/app/[locale]/category/[...tag]/page.tsx
@@ -1,16 +1,14 @@
-import { AspectRatio, Button, Container, Grid, GridCol, Title } from '@mantine/core'
-import { Image } from '~/app/_components/Image'
+import { Container } from '@mantine/core'
 import Link from 'next/link'
 import { initTranslations } from '~/app/i18n'
 import { api } from '~/trpc/server'
 import { notFound } from 'next/navigation'
 import { StoryPreviewCard } from '~/app/_components/StoryPreviewCard'
-import Logo from '~public/assets/tmf-logo-rect-bw-cropped.png'
 import { type Metadata } from 'next'
-import { fontIbmPlexSans } from '~/app/_styles/fonts'
 import { getCategoryImage } from '~/data/categoryImages'
 import { CardDisplay } from '~/app/_components/CardDisplay'
 import { IndividualStory } from '~/app/_components/IndividualStory'
+import { GalleryBanner, type StoryCategories } from '~/app/_components/GalleryBanner'
 
 export const generateMetadata = async ({ params: { locale } }: PageProps): Promise<Metadata> => {
 	const { t } = await initTranslations(locale, ['common'])
@@ -18,9 +16,7 @@ export const generateMetadata = async ({ params: { locale } }: PageProps): Promi
 		title: t('page-title.general-template', { page: t('nav.stories') }),
 	}
 }
-const CategoryPage = async ({ params: { locale, tag: tagParams } }: PageProps) => {
-	const { t } = await initTranslations(locale, ['common', 'stories'])
-
+const CategoryPage = async ({ params: { tag: tagParams } }: PageProps) => {
 	const [tag, storyId] = tagParams
 	if (!tag) {
 		return notFound()
@@ -50,30 +46,7 @@ const CategoryPage = async ({ params: { locale, tag: tagParams } }: PageProps) =
 
 	return (
 		<Container fluid>
-			<Grid align='center' my='md' grow>
-				<GridCol span={{ sm: 6, md: 4, lg: 5 }} px={{ lg: 40 }}>
-					<AspectRatio ratio={723 / 174} my={40} mx='auto' maw={750}>
-						<Image src={Logo} alt={t('logo-alt')} />
-					</AspectRatio>
-				</GridCol>
-				<GridCol span={{ sm: 6, md: 4, lg: 4 }}>
-					<Title fw={100} order={1} fz={30} ta='center' tt='uppercase' style={fontIbmPlexSans.style} lts={5}>
-						{t(`story-gallery.${tag}`)}
-					</Title>
-				</GridCol>
-				<GridCol span={{ lg: 3, md: 4 }}>
-					<Button
-						component={Link}
-						href={{ pathname: '/survey' }}
-						tt='uppercase'
-						variant='secondary'
-						display='block'
-						mx='auto'
-					>
-						{t('participate')}
-					</Button>
-				</GridCol>
-			</Grid>
+			<GalleryBanner pageTitle={`story-gallery.${tag as StoryCategories}`} />
 			{Boolean(previewCards.length) && <CardDisplay>{previewCards}</CardDisplay>}
 
 			{singleStory && (
--- src/app/[locale]/gallery/page.tsx
+++ src/app/[locale]/gallery/page.tsx
@@ -3,13 +3,11 @@ import type { Metadata } from 'next'
 import { Image } from '~/app/_components/Image'
 import { artData } from '~/data/artwork'
 
-import { fontIbmPlexSans } from '~/app/_styles/fonts'
-import Logo from '~public/assets/tmf-logo-rect-bw-cropped.png'
-
-import { AspectRatio, Button, Container, Grid, GridCol, Stack, Title, rem } from '@mantine/core'
+import { AspectRatio, Container, Grid, GridCol, Stack, Title, rem } from '@mantine/core'
 import { StoryPreviewCarousel } from '~/app/_components/StoryPreviewCarousel'
 
 import { PopupArt } from '~/app/_components/PopupArt'
+import { GalleryBanner } from '~/app/_components/GalleryBanner'
 import { initTranslations } from '~/app/i18n'
 import classes from './page.module.css'
 
@@ -85,34 +83,9 @@ const Page = async ({ params: { locale } }: PageProps) => {
 		</Stack>
 	))
 
-	const logoAltText = t('logo-alt')
-
 	return (
 		<Container fluid>
-			<Grid align='center' my='md' grow>
-				<GridCol span={{ sm: 6, lg: 5, md: 4 }} px={{ lg: 40 }}>
-					<AspectRatio ratio={723 / 174} my={40} mx='auto' maw={750}>
-						<Image src={Logo} alt={logoAltText} fit='contain' />
-					</AspectRatio>
-				</GridCol>
-				<GridCol span={{ sm: 6, md: 4 }}>
-					<Title fw={300} order={1} fz={30} ta='center' tt='uppercase' lts={5} style={fontIbmPlexSans.style}>
-						{t('page-title.gallery')}
-					</Title>
-				</GridCol>
-				<GridCol span={{ base: 6, lg: 3, md: 4 }}>
-					<Button
-						component={Link}
-						href={{ pathname: '/survey' }}
-						tt='uppercase'
-						variant='secondary'
-						display='block'
-						mx='auto'
-					>
-						{t('participate')}
-					</Button>
-				</GridCol>
-			</Grid>
+			<GalleryBanner pageTitle='page-title.gallery' />
 			<StoryPreviewCarousel slidesToScroll='auto' slideSize='33%' visibleFrom='md'>
 				{slides}
 			</StoryPreviewCarousel>
